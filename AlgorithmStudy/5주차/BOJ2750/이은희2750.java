5fd5063e3f6441ce3d7039816d2422716cd351e4
qscv06532 <qscv06532@gmail.com>
2025-12-04 09:08:04 +0900

    Add files via upload

---
+++ AlgorithmStudy/5주차/BOJ2750/이은희2750.java
@@ -0,0 +1,27 @@
+package algorithm.study.day05;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Asc {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int a = Integer.parseInt(br.readLine());
+		
+		int[] sort = new int[a];
+		
+		for(int i = 0; i < sort.length; i++) {
+			sort[i] = Integer.parseInt(br.readLine());
+		}
+		
+		Arrays.sort(sort);
+		
+		for(int data : sort) {
+			System.out.println(data);
+		}
+	}
+
+}
