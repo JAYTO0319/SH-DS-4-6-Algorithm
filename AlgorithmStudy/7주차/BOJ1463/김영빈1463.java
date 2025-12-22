e68f71837d32b273681a3b3396531952f80bd14a
KIM YONUGBEEN <81636454+sttarrynight@users.noreply.github.com>
2025-12-22 09:05:33 +0900

    Create 김영빈1463.java

---
+++ AlgorithmStudy/7주차/BOJ1463/김영빈1463.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main1463 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		int[] arr = new int[N+1];
+		
+		for(int i=2; i<=N; i++) {
+			int r1 = N;
+			int r2 = N;
+			int r3 = N;
+			
+			if(i%3==0) {
+				r1 = arr[i/3];
+			}
+			
+			if(i%2==0) {
+				r2 = arr[i/2];
+			}
+			
+			r3 = arr[i-1];
+			
+			int min = Math.min(Math.min(r1, r2), r3);
+			arr[i] = min + 1;
+		}
+		
+		System.out.println(arr[N]);
+	}
+
+}
