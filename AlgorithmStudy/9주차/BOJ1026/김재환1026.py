73b7b62dacf595eaa398a7279185691ac2ff4fb1
JAYTO0319 <ktkk2246@gmail.com>
2025-12-30 09:21:27 +0900

    Update 김재환1026.py

--- AlgorithmStudy/9주차/BOJ1026/김재환1026.py
+++ AlgorithmStudy/9주차/BOJ1026/김재환1026.py
@@ -1,50 +1,29 @@
 import sys
-from collections import deque
-
 input = sys.stdin.readline
 
-# 상하좌우 이동 방향
-dr = [-1, 1, 0, 0]
-dc = [0, 0, -1, 1]
+# N: 배열 길이
+N = int(input().strip())
 
-T = int(input().strip())
-for _ in range(T):
-    # M: 가로 길이, N: 세로 길이, K: 배추 개수
-    M, N, K = map(int, input().split())
+# A: 재배열 가능한 배열
+A = list(map(int, input().split()))
 
-    # 배추 위치 표시 배열: 0 = 없음, 1 = 있음
-    field = [[0]*M for _ in range(N)]
-    for __ in range(K):
-        x, y = map(int, input().split())
-        field[y][x] = 1
+# B: 문제 설명상 재배열하면 안 되는 배열
+# 하지만 "최소 합"을 계산하는 데는,
+# B의 값들을 어떤 순서로 대응시키는지가 중요할 뿐,
+# B 자체를 출력하거나 인덱스를 실제로 바꿔 제출하는 게 아니다.
+B = list(map(int, input().split()))
 
-    # 방문 체크
-    visited = [[False]*M for _ in range(N)]
+# 핵심 그리디:
+# 1) A는 작은 값부터
+# 2) B는 큰 값부터
+# 이렇게 1:1로 곱해 더하면 전체 합이 최소가 된다.
+A.sort()
+B.sort(reverse=True)
 
-    def bfs(sr, sc):
-        queue = deque()
-        queue.append((sr, sc))
-        visited[sr][sc] = True
+# 최소 합 계산
+# (A[i]는 작은 것부터, B[i]는 큰 것부터 매칭)
+ans = 0
+for i in range(N):
+    ans += A[i] * B[i]
 
-        while queue:
-            r, c = queue.popleft()
-            for i in range(4):
-                nr = r + dr[i]
-                nc = c + dc[i]
-                # 범위 체크
-                if 0 <= nr < N and 0 <= nc < M:
-                    # 배추 있고 아직 방문 안 했으면
-                    if field[nr][nc] == 1 and not visited[nr][nc]:
-                        visited[nr][nc] = True
-                        queue.append((nr, nc))
-
-    # 군집 수 세기
-    count = 0
-    for r in range(N):
-        for c in range(M):
-            if field[r][c] == 1 and not visited[r][c]:
-                # 새 연결 요소 발견
-                bfs(r, c)
-                count += 1
-
-    print(count)
+print(ans)
